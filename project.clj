3027e15eff02d0579f14599fe5b5fcd389758a2f
Ghadi Shayban <gshayban@gmail.com>
2015-10-14 16:56:37 -0400

    initial

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject com.pokitdok/pokitdok-api "0.0.1"
+  :description "A Pokitdok API client"
+  :url "http://example.com/FIXME"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-http "2.0.0"]])
