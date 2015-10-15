39c058dc8960ec8bdb7abb4513d7737472b0b659
Ghadi Shayban <gshayban@gmail.com>
2015-10-14 23:02:58 -0400

    add dependencies and basic oauth

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-http "2.0.0"]])
+                 [clj-http "2.0.0"]
+                 [commons-codec "1.10"]
+                 [ring/ring-codec "1.0.0"]
+                 [cheshire "5.5.0"]])
--- src/com/pokitdok/client.clj
+++ src/com/pokitdok/client.clj
@@ -1,2 +1,39 @@
-(ns com.pokitdok.client)
+(ns com.pokitdok.client
+  (:require [clj-http.client :as http]
+            [cheshire.core :as json])
+  (:import [org.apache.commons.codec.binary Base64]))
 
+(def DEFAULT-API-BASE "https://platform.pokitdok.com")
+(def DEFAULT-SCOPE "default")
+(def USER-SCHEDULE-SCOPE "user_schedule")
+
+(def default-api-version "v4")
+
+(def pokitdok-client-version "0.0.1")
+(def default-headers {"User-Agent" (format "pokitdok-clj %s JDK"
+                                           pokitdok-client-version
+                                           (System/getProperty "java.version"))})
+
+(defn api-url
+  "Endpoint must have a leading /"
+  [api-base version endpoint]
+  (str api-base "/api/" version endpoint))
+
+(defn make-auth-header
+  [client-id client-secret]
+  {"Authorization" (Base64/encodeBase64String (str client-id ":" client-secret))})
+
+(defn authorize
+  ([client-id client-secret]
+    (authorize DEFAULT-API-BASE))
+  ([api-base client-id client-secret]
+   (let [auth-endpoint (str api-base "/oauth2/token")
+         auth-header (make-auth-header client-id client-secret)
+
+         params {:form-params {"grant_type" "client_credentials"}
+                 :headers (merge default-headers auth-header)
+                 :as :json}
+
+         response (http/get auth-endpoint params)]
+
+     {:oauth-token (get-in response [:body :access_token])})))
