eca48d574ce2a9e2765e7961777fbeb2d9884b29
Ghadi Shayban <gshayban@gmail.com>
2015-11-10 14:32:42 -0500

    fixup project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
 (defproject com.pokitdok/pokitdok-api "0.0.1"
-  :description "A Pokitdok API client"
-  :url "http://example.com/FIXME"
+  :description "A PokitDok API client"
+  :url "http://github.com/pokitdok/pokitdok-clojure"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http "2.0.0"]
                  [commons-codec "1.10"]
                  [ring/ring-codec "1.0.0"]
-                 [cheshire "5.5.0"]]
-  :profiles {:dev {:dependencies [[clj-http-fake "1.0.1"]]}})
+                 [cheshire "5.5.0"]])
